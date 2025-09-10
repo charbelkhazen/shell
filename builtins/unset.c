8ef4940f465e48d9833d28399f11e4b8f32d2fd1
Charbel Khazen <khazencharbel@gmail.com>
2025-09-10 17:48:36 +0300

    unset comply with norm

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,105 +6,69 @@
 /*   By: jissa <jissa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 19:24:37 by jissa             #+#    #+#             */
-/*   Updated: 2025/09/09 18:31:10 by chkhazen         ###   ########.fr       */
+/*   Updated: 2025/09/10 17:47:41 by chkhazen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-/*
-int	unset_builtin(char **args, char **env)
+void	utilremoveenvvar(int *i, char ***env, char ***orig)
 {
-	int	i;
-	int	j;
-
-	if (!args[1])
-		return (0);
-	i = 0;
-	while (env[i])
-	{
-		if (strncmp(env[i], args[1], strlen(args[1])) == 0 \
-&& env[i][strlen(args[1])] == '=')
-		{
-			j = i;
-			while (env[j])
-			{
-				env[j] = env[j + 1];
-				j++;
-			}
-			continue ;
-		}
-		i++;
-	}
-	return (0);
+	*i = 0;
+	while (*(*env + (*i)))
+		(*i)++;
+	*orig = *env;
 }
-*/
 
 void	removeenvvar(char *arg, char ***env, char **foundenv)
 {
-	int	i;
+	int		i;
 	char	**orig;
-	int	index;
-	int	j;
+	int		index;
+	int		j;
 
-	index = (int) (foundenv - *env);
-	//printf("DIFF:%d\n", index);
+	index = (int)(foundenv - *env);
+	utilremoveenvvar(&i, env, &orig);
+	*env = malloc(sizeof(char *) * i);
 	i = 0;
-	while (*(*env + i))
-                i++;
-        orig = *env;
-        *env = malloc(sizeof(char *) * i);
-        i = 0;
 	while (i < index)
 	{
-		//printf("i: %d\n", i);
-                *(*env + i) = ft_strdup(*(orig + i));
+		*(*env + i) = ft_strdup(*(orig + i));
 		i ++;
 	}
 	j = i;
 	if (*(orig + i))
-	{
-		//printf("i: %d\n", i);
 		i ++;
-	}
-        while (*(orig + i))
-        {
-		//printf("i: %d, j: %d\n", i, j);
-                *(*env + j) = ft_strdup(*(orig + i));
-                i++;
+	while (*(orig + i))
+	{
+		*(*env + j) = ft_strdup(*(orig + i));
+		i++;
 		j++;
-        }
-        *(*env + j) = 0;
-        freeenv(orig);
+	}
+	*(*env + j) = 0;
+	freeenv(orig);
 }
 
-void    applyunset(char *arg, char ***env)
+void	applyunset(char *arg, char ***env)
 {
-        char    **foundenv;
+	char	**foundenv;
 
-        foundenv = findinenv(arg, *env);
+	foundenv = findinenv(arg, *env);
 	if (foundenv)
 		removeenvvar(arg, env, foundenv);
 }
 
-int     unset_builtin(char **args, char ***env)
+int	unset_builtin(char **args, char ***env)
 {
-        char    *var;
+	char	*var;
 
-        if (!args[1])
-                return (0);
-        args++;
-        while (*args)
+	if (!args[1])
+		return (0);
+	args++;
+	while (*args)
 	{
 		applyunset(*args, env);
 		args++;
 	}
-	/*
-	while (**env)
-	{
-		printf(">%s\n", **env);
-		(*env)++;
-	}
-	*/
 	return (0);
 }
