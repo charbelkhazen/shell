20dfac4b9a7767499bc119876c468030cf36b8ed
Jihad Issa <jissa@c2r1s1.42beirut.com>
2025-09-16 15:34:19 +0300

    export is sorted and quotes are added among the value

--- builtins/export1.c
+++ builtins/export1.c
@@ -3,20 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   export1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chkhazen <chkhazen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jissa <jissa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 16:44:55 by chkhazen          #+#    #+#             */
-/*   Updated: 2025/09/10 17:28:42 by chkhazen         ###   ########.fr       */
+/*   Updated: 2025/09/15 17:24:51 by jissa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	sort_envp(char **envp)
+{
+	int	i;
+	int	j;
+	char	*temp;
+
+	i = 0;
+	while (envp[i])
+	{
+		j = i + 1;
+		while (envp[j])
+		{
+			if (ft_strcmp(envp[i], envp[j]) > 0)
+			{
+				temp = envp[i];
+				envp[i] = envp[j];
+				envp[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+char	*quoted_value(char *value)
+{
+	char	*tmp;
+	char	*res;
+
+	tmp = ft_strjoin("\"", value);
+	res = ft_strjoin(tmp, "\"");
+	free(tmp);
+	return (res);
+}
+
+void	add_quotes(char **envp)
+{
+	int	i;
+	char	*value;
+	char	*new_entry;
+	char	*final_value;
+	char	*extracted_var;
+
+	i = 0;
+	while (envp[i])
+	{
+		value = ft_strchr(envp[i], '=');
+		if (value)
+		{
+			extracted_var = ft_substr(envp[i], 0, (ft_strlen(envp[i]) - ft_strlen(value)) + 1);
+			final_value = quoted_value(value + 1);
+			new_entry = ft_strjoin(extracted_var, final_value);
+			free(extracted_var);
+			free(final_value);
+			free(envp[i]);
+			envp[i] = new_entry;
+		}
+		i++;
+	}
+}
 
 void	handle_exportcmd(char **envp)
 {
 	int	i;
 
 	i = 0;
+	sort_envp(envp);
+	add_quotes(envp);
 	while (envp[i])
 	{
 		printf("declare -x %s\n", envp[i]);
