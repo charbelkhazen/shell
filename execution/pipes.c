1841666300584f5b57990b4381dedd5026b49cde
Charbel Khazen <khazencharbel@gmail.com>
2025-09-24 11:29:13 +0300

    hardcoded heredoc priority in pipes

--- execution/pipes.c
+++ execution/pipes.c
@@ -6,18 +6,32 @@
 /*   By: jissa <jissa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 10:43:40 by jissa             #+#    #+#             */
-/*   Updated: 2025/09/16 14:29:21 by chkhazen         ###   ########.fr       */
+/*   Updated: 2025/09/24 11:16:07 by chkhazen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	ishdinleft(t_tree *tree)
+{
+	while (tree ->type == '<' || tree -> type == '>' \
+		 || tree ->type == 'h' || tree ->type == 'a')
+	{
+		if (tree -> type == 'h')
+			return (1);
+		tree = ((t_redirtree *)tree) -> cmd;
+	}
+	return (0);
+}
+
 void	exec_pipe(t_pipetree *tree, char ***envp, int *status)
 {
 	int		pipefd[2];
+	int		heredocflag;
 	pid_t	pid1;
 	pid_t	pid2;
 
+	heredocflag = ishdinleft(tree -> left);
 	pipe(pipefd);
 	pid1 = fork();
 	if (pid1 == 0)
@@ -27,6 +41,8 @@ void	exec_pipe(t_pipetree *tree, char ***envp, int *status)
 		close(pipefd[1]);
 		trav_tree(tree->left, envp, status);
 	}
+	if (heredocflag)
+		waitpid(pid1, status, 0);
 	pid2 = fork();
 	if (pid2 == 0)
 	{
